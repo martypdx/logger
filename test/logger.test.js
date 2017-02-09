cff1b162c96ac214580f143d29f7e58a1fd33c21
martypdx <marty@martypdx.com>
2017-02-09 15:17:58 -0800

    add test for console.log default

--- test/logger.test.js
+++ test/logger.test.js
@@ -4,15 +4,18 @@ const createLogger = require('../lib/logger');
 describe('logging middleware', () => {
 
     it('logs request method and url, then calls next', done => {
-
         let logged = '';
-        const logger = createLogger(log => logged = log);
+        let log = l => logged = l;
+        const logger = createLogger(log);
+        assert.equal(logger.log, log);
 
         const req = { method: 'GET', url: '/tests' };
-
         logger(req, null, done);
-
         assert.equal(logged, 'GET /tests');
+    });
 
+    it('uses console.log by default', () => {
+        const logger = createLogger();
+        assert.equal(logger.log, console.log);
     });
 });
